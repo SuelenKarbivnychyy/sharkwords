ada3e53fb7cf7f130a22c4ed6dd31ef1a6a210e5
Suelen Karbivnychyy <suelenmatosr@gmail.com>
2024-02-05 18:52:49 -0500

    Developed user interface, Added alphabet, ability for the letter to be disabled and added empty lines do the guess

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -24,10 +24,10 @@ const createDivsForChars = (word) => {
     let item = `<div class="letter-box ${letter}"></div>`;
     document.querySelector('#word-container').insertAdjacentHTML('beforeend', item);
   }
+  console.log(word);
 };
 
-// Loop over each letter in the alphabet and generate a button for each letter
-// The buttons should be appended to the section with id="letter-buttons".
+
 const generateLetterButtons = () => {
   
   let container = document.querySelector('#letter-buttons');
@@ -37,12 +37,9 @@ const generateLetterButtons = () => {
   }
 };
 
-// Set the `disabled` property of `buttonEl` to true.
-//
-// `buttonEl` is an `HTMLElement` object.
-//
+
 const disableLetterButton = (buttonEl) => {
-  // Replace this with your code
+  buttonEl.setAttribute('disabled', 'true');  
 };
 
 // This is a helper function we will use in the future
@@ -50,7 +47,13 @@ const disableLetterButton = (buttonEl) => {
 // For now, you should test it out to make sure it works
 
 const isLetterInWord = (letter, word) => {
-  // Replace this with your code
+
+  if (word.includes(letter)) {
+    return true;
+  } else {
+    return false;
+  }
+
 };
 
 // This is like if __name__ == '__main__' in Python
@@ -61,7 +64,11 @@ const isLetterInWord = (letter, word) => {
   // You can change this to choose a random word from WORDS once you
   // finish this lab but we hard code it so we know what the word is
   // and can tell if things look correct for this word
-  const word = 'hello';
+
+
+  // const word = 
+  const word = WORDS[Math.floor(Math.random() * WORDS.length)];
+ 
 
   createDivsForChars(word);
 
