d3b19c64dbf2e69ad7d73dd885a46dc9ffb7ae90
Suelen Karbivnychyy <suelenmatosr@gmail.com>
2024-02-06 15:04:02 -0500

    Developed handleCorrectGuess function to diplay the correct guess from user when a letter is correct guessed

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -28,6 +28,15 @@ const createDivsForChars = (word) => {
 };
 
 
+const handleCorrectGuess = (letter) => {
+  
+  const wordLetter = document.querySelectorAll(`.${letter}`);
+  for (item of wordLetter) {
+    item.innerHTML = letter;
+  }
+}
+
+
 const generateLetterButtons = () => {
   
   let container = document.querySelector('#letter-buttons');
@@ -42,37 +51,31 @@ const disableLetterButton = (buttonEl) => {
   buttonEl.setAttribute('disabled', 'true');  
 };
 
-// This is a helper function we will use in the future
-// It should return `true` if `letter` is in the word
-// For now, you should test it out to make sure it works
 
 const isLetterInWord = (letter, word) => {
 
   if (word.includes(letter)) {
-    return true;
+    handleCorrectGuess(letter);  
   } else {
     return false;
   }
 
 };
 
-// This is like if __name__ == '__main__' in Python
-// It will be called when the file is run (because
-// we call the function on line 66)
+
+
 (function startGame() {
-  // For now, we'll hardcode the word that the user has to guess
-  // You can change this to choose a random word from WORDS once you
-  // finish this lab but we hard code it so we know what the word is
-  // and can tell if things look correct for this word
-
-
-  // const word = 
+  
   const word = WORDS[Math.floor(Math.random() * WORDS.length)];
  
 
   createDivsForChars(word);
 
-  generateLetterButtons();
+  const buttons = generateLetterButtons();
+
+  // for (letter of buttons) {
+
+  // }
 
   // in the next lab, you will be adding functionality to handle when
   // someone clicks on a letter
